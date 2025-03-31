445544542b07e18fae05b396deea991e6d82c7e2
DuyHixd1234 <ngohuynhduyhaiqn@gmail.com>
2025-03-31 08:41:30 +0700

    Lab4

---
+++ Assets/ScriptsScene/Choilai.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Choilai : MonoBehaviour
+{
+    public void ChoiTiep()
+    {
+        SceneManager.LoadScene("SampleScene"); // Chuyển sang Lab4
+    }
+}
